695ac77a57e2c2a82d49ad40c8b4403a9a81810e
sizarcorpse <sizarcorpse@gmail.com>
2021-01-22 23:53:04 +0600

    v05.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -86,7 +86,7 @@ const Home = (props) => {
 
   return (
     <Grid container components="main" className={localClasses.backgroundColor}>
-      <Grid item xs={12} className={classes.demo}>
+      <Grid item xs={12}>
         <Box style={{ display: "flex", justifyContent: "center" }}>
           <SwiperRoot editorChoices={editorChoices} />
         </Box>
