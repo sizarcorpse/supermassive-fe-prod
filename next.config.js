2a29f9c3b753c947fc59a2af95985ada06696815
sizarcorpse <sizarcorpse@gmail.com>
2021-01-22 23:28:19 +0600

    v05.1

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@ const path = require("path");
 module.exports = {
   publicRuntimeConfig: {
     ROOT_API_URL: process.env.REACT_APP_ROOT_API_URL || "http://localhost:1337",
-    ROOT_IMAGE_URL:
-      process.env.REACT_APP_ROOT_IMAGE_API_URL || '"http://localhost:1337"',
   },
 
-  images: {
-    domains: ["sc-supermassive.s3.amazonaws.com", "localhost"],
-  },
   webpack: (config) => {
     config.resolve.alias["components"] = path.join(
       __dirname,
